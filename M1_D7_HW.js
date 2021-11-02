af8f4728408de3d4a7c5b339ac3749217b92c91d
Anudeep <36949642+anucherukuri@users.noreply.github.com>
2021-11-02 16:08:56 +0000

    Updated M1-D7-HW files

---
+++ M1_D7_HW.js
@@ -0,0 +1,31 @@
+
+ 
+
+// …from 7 to 12, should be written in JavaScript:
+
+ 
+
+// EX7: Write a function to change the h1 text to something else
+
+
+const changeH1 = function() {
+    let changeH1Node = document.getElementById("h1_id")
+    changeH1Node.innerText = "Anu's Shopiee"
+    }
+    changeH1()
+
+
+
+
+// EX8: Write a function to change the page background color
+
+document.body.style.backgroundColor = "blue"
+
+// EX9: Write a function to change the footer address with a fake one
+
+
+// EX10: Write a function to add a CSS class to every Amazon link
+
+// EX11: Write a function to toggle a CSS class for all the images in the table; that class should set the visibility of the image
+
+// EX12: Write a function to color the price of the products in a different one every time it's invoked
